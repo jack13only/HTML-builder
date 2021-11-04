1e2da2a7a6bbe678643f3ef81ac19de30a71c036
jack13only <jack13only@gmail.com>
2021-11-04 11:46:48 +0300

    fix: fix actual copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,11 +3,24 @@ const path = require('path');
 const stream = require('stream');
 const fsPromises = fs.promises;
 
+
 function createDir() {
     fsPromises.mkdir(path.join(__dirname, 'files-copy'))
-          .then(function() {})
           .catch(function() {
             console.log('Directory has already been created');
+          })
+}
+
+function copyDir() {
+    fsPromises.rmdir(path.join(__dirname, 'files-copy'), { recursive: true, force: true })
+          .then(() => {
+            createDir()
+          })
+          .then(() => {
+            readDir()
+          })
+          .catch(function() {
+            console.log('Something go wrong');
           })
 }
 
@@ -15,10 +28,9 @@ function copyToFile(file) {
     try {
         fsPromises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
     } catch {
-        console.log('The file could not be copied');
+        console.log('Copy error');
     }
 }
-
 
 function readDir() {
     fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
@@ -28,5 +40,4 @@ function readDir() {
     )
 }
 
-createDir()
-readDir()
+copyDir()
